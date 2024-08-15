e32792d7c94c99cfd408eb2b046bee73a0c08779
Augustin Diabira <augustindiabira@gmail.com>
2024-08-16 00:40:02 +0200

    Plummy API: token types & expiration

--- lib/plummy_api_web/auth/error_response.ex
+++ lib/plummy_api_web/auth/error_response.ex
@@ -5,3 +5,7 @@ end
 defmodule PlummyApiWeb.Auth.ErrorResponse.Forbidden do
   defexception message: "You do not have permission to perform this action.", plug_status: 403
 end
+
+defmodule PlummyApiWeb.Auth.ErrorResponse.NotFound do
+  defexception message: "Not Found", plug_status: 404
+end
--- lib/plummy_api_web/auth/guardian.ex
+++ lib/plummy_api_web/auth/guardian.ex
@@ -14,7 +14,7 @@ defmodule PlummyApiWeb.Auth.Guardian do
   def resource_from_claims(%{"sub" => id}) do
     case Accounts.get_account!(id) do
       nil -> {:error, :not_found}
-      ressource -> {:ok, ressource}
+      resource -> {:ok, resource}
     end
   end
 
@@ -25,13 +25,21 @@ defmodule PlummyApiWeb.Auth.Guardian do
   def authenticate(email, password) do
     case Accounts.get_account_by_email(email) do
       nil ->
-        {:error, :no_account}
+        {:error, :unauthored}
 
       account ->
         case validate_password(password, account.hash_password) do
-          true -> create_token(account)
+          true -> create_token(account, :access)
           false -> {:error, :unauthorized}
         end
+    end
+  end
+
+  def authenticate(token) do
+    with {:ok, claims} <- decode_and_verify(token),
+         {:ok, account} <- resource_from_claims(claims),
+         {:ok, _old, {new_token, _claims}} <- refresh(token) do
+      {:ok, account, new_token}
     end
   end
 
@@ -39,9 +47,17 @@ defmodule PlummyApiWeb.Auth.Guardian do
     Bcrypt.verify_pass(password, hash_password)
   end
 
-  defp create_token(account) do
-    {:ok, token, _claims} = encode_and_sign(account)
+  defp create_token(account, type) do
+    {:ok, token, _claims} = encode_and_sign(account, %{}, token_options(type))
     {:ok, account, token}
+  end
+
+  defp token_options(type) do
+    case type do
+      :access -> [token_type: "access", ttl: {2, :hour}]
+      :reset -> [token_type: "reset", ttl: {15, :minute}]
+      :admin -> [token_type: "admin", ttl: {90, :day}]
+    end
   end
 
   def after_encode_and_sign(resource, claims, token, _options) do
@@ -53,6 +69,12 @@ defmodule PlummyApiWeb.Auth.Guardian do
   def on_verify(claims, token, _options) do
     with {:ok, _} <- Guardian.DB.on_verify(claims, token) do
       {:ok, claims}
+    end
+  end
+
+  def on_refresh({old_token, old_claims}, {new_token, new_claims}, _options) do
+    with {:ok, _, _} <- Guardian.DB.on_refresh({old_token, old_claims}, {new_token, new_claims}) do
+      {:ok, {old_token, old_claims}, {new_token, new_claims}}
     end
   end
 
